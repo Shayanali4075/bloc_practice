8f4891665bbf28390d7e3c23fadada4bf1ea4782
Shayanali4075 <92671813+Shayanali4075@users.noreply.github.com>
2024-01-26 14:38:15 +0500

    api integrated using bloc

--- lib/view/Apiscreen.dart
+++ lib/view/Apiscreen.dart
@@ -1,11 +1,24 @@
 import 'package:bloc_practice/bloc/api/apibloc.dart';
+import 'package:bloc_practice/bloc/api/apievent.dart';
 import 'package:bloc_practice/bloc/api/apistate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ApiScreen extends StatelessWidget {
+class ApiScreen extends StatefulWidget {
   const ApiScreen({super.key});
-  
+
+  @override
+  State<ApiScreen> createState() => _ApiScreenState();
+}
+
+class _ApiScreenState extends State<ApiScreen> {
+
+  @override
+  void initState() {
+    context.read<ApiBloc>().add(GetApiEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
